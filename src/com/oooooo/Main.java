3d867ab06802a8831d2092ee23927fb60d382076
ywk <17673053954@163.com>
2024-09-06 14:32:43 +0800

    我100

--- src/com/oooooo/Main.java
+++ src/com/oooooo/Main.java
@@ -12,5 +12,6 @@ public class Main {
         System.out.println("我007");
         System.out.println("jobuvs008");
         System.out.println("jobuvs009");
+        System.out.println("我100");
     }
 }
